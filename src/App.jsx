8317e99e1791b8c160e65076e0ab47a5713f7ace
Fran3103 <fn.aguirre@hotmail.com>
2024-03-14 19:22:57 -0300

    arreglos

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Navbar from './Components/Navbar'
 import Home from './Components/Home'
 import Footer from './Components/Footer'
 import Stories from './Components/Stories'
+import Features from './Components/Features'
+import { Pricing } from './Components/Pricing'
 
 
 function App() {
@@ -17,6 +19,8 @@ function App() {
      <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/stories' element={<Stories/>} />
+        <Route path='/features' element={<Features/>} />
+        <Route path='/pricing' element={<Pricing/>} />
      </Routes>
      <Footer/>
     </>
